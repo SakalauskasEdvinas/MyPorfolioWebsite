67499c470b3d48ea8cd014c2521ea83137fca9bc
= <edvin.sakalauskas@gmail.com>
2023-04-16 16:57:15 +0100

    redesign and fade in effect

--- js/script.js
+++ js/script.js
@@ -1,71 +1,73 @@
 
-const body = document.querySelector('body');
-const modeToggle = document.getElementById('mode-toggle');
-const modeStatus = document.querySelector('.mode-status');
-function toggleMode() {
-body.classList.toggle('dark-mode');
-
-const modeMessage = body.classList.contains('dark-mode') ?
-    'Dark Mode'
-    : "Light Mode"
-
-  }
-
-modeToggle.addEventListener('click', toggleMode);
+window.addEventListener("scroll",fade );
+window.addEventListener("load",fade );
 
 
-//main logic
-let currentIndex = 0;
-const prevButton = document.getElementById("prev");
-const nextButton = document.getElementById("next");
-const images = document.querySelectorAll("#slider img");
-console.log(images);
-//set first image as visible
-images[currentIndex].classList.add("active");
-console.log(images[0]);
+function fade() {
+  const fadeIns = document.querySelectorAll('.fade-in');
 
-//add event listeners to each button
-//argument -1 to go to previous image, 1 to go to the next
-prevButton.addEventListener("click", () => {
-  changeSlide(-1)
-});
-nextButton.addEventListener("click", () => {
-  changeSlide(1)
-});
-
-
-
-
-//change slide function
-function changeSlide(indexModifier) {
-    if(indexModifier === null) {
-        indexModifier = 1;
-    }
-  let newIndex = currentIndex + indexModifier;
-  if(newIndex >= images.length) {
-    //if index number is bigger than the img node collection, set index to the first image
-    newIndex = 0;
-  } else if(newIndex < 0) {
-    //if index is negative, set index to the last image
-    newIndex = images.length - 1;
-  }
-    
-  //Hide the current image
-  images[currentIndex].classList.remove("active");
-  console.log(currentIndex);
-
-  //Show the next or prev image
-   images[newIndex].classList.add("active");
-
+  const options = {
+    rootMargin: '0px',
+    threshold: 0.5
+  };
   
-  //set the new currentIndex
-  currentIndex = newIndex;
-
- 
- 
+  const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add('active');
+      } else {
+        entry.target.classList.remove('active');
+      }
+    });
+  }, options);
+  
+  fadeIns.forEach(fadeIn => {
+    observer.observe(fadeIn);
+  });
 }
 
-setInterval(() => {
-    changeSlide(null);
-}, 3000);
+
+var typed = new Typed('#text', {
+strings: ['PORTFOLIO'],
+typeSpeed: 20,
+showCursor: true
+});
+
+
+
+const swiper = new Swiper(".swiper-container", {
+  // Optional parameters
+  slidesPerView: 2,
+  spaceBetween: 0,
+  direction: "horizontal",
+  loop: true,
+  speed:600,
+  breakpoints: {
+    // when window width is >= 320px
+    320: {
+     slidesPerView: 1,
+     spaceBetween: 0
+    },
+    480: {
+      slidesPerView: 1,
+      spaceBetween: 0
+     },
+    900: {
+      slidesPerView: 2,
+      spaceBetween: 0
+     }
+    
+  },
+  // If you need pagination
+  pagination: {
+      el: ".swiper-pagination",
+      clickable: true
+  },
+
+  // Navigation arrows
+  navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+  },
+});
 
